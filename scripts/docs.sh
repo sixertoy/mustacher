917a21fe684828a8cbea4fdf57bbaacb4773e9a2
sixertoy <sixertoy.github@gmail.com>
2015-08-24 10:01:55 +0200

    remove docs + bump version

--- scripts/docs.sh
+++
@@ -1,3 +0,0 @@
-mdown --input "./docs/**/*.md" --output ./build/docs --header "./docs/assets/header.html" --footer "./docs/assets/footer.html"
-github-markdown-css > ./build/docs/github-markdown.css
-exit 0
