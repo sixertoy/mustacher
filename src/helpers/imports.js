6e28a5655b1638422e14c82b016dc411420559c6
sixertoy <sixertoy.github@gmail.com>
2016-09-21 14:00:46 +0200

    0.1.20 - fix JS import CDATA - correst eslint in imports.js - add minification to JS and CSS

--- src/helpers/imports.js
+++ src/helpers/imports.js
@@ -9,29 +9,42 @@
  * @see http://handlebarsjs.com/
  *
  */
-/*jslint indent: 4, nomen: true */
-/*global module, require, console */
-(function () {
+/* global module, require, console */
+(function() {
 
     'use strict';
 
     var ImportsHelper,
-        js_ext = '.js',
-        css_ext = '.css',
+        JS_EXTENSION = '.js',
+        CSS_EXTENSION = '.css',
         fs = require('fs'),
+        uglify = require('uglify-js'),
+        CleanCSS = require('uglify-js'),
         Handlebars = require('handlebars'),
-        mustacher = require('./../mustacher');
+        mustacher = require('./../mustacher'),
+        isboolean = require('lodash.isboolean');
 
-    ImportsHelper = function () {};
+    ImportsHelper = function() {};
 
     /**
-     * 
-     * 
-     * 
+     *
+     *
+     *
      */
-    ImportsHelper.prototype.getInlineContent = function (file) {
+    ImportsHelper.prototype.getInlineContent = function(file, iscss, compress) {
+        var content;
         try {
-            return fs.readFileSync(file, 'utf8');
+            content = fs.readFileSync(file, 'utf8');
+            if (!compress) {
+                return content;
+            }
+            if (iscss) {
+                return new CleanCSS().minify(content).styles;
+            }
+            return uglify(content, {
+                fromString: true
+            });
+
         } catch (e) {
             console.log('unable to load file ' + file);
             return '/* unable to load file ' + file + ' */';
@@ -39,70 +52,86 @@
     };
 
     /**
-     * 
-     * 
-     * 
+     *
+     *
+     *
      */
-    ImportsHelper.prototype.injectJS = function (file, inline) {
+    ImportsHelper.prototype.injectJS = function(file, inline, compress) {
         var result;
         if (!inline) {
-            result = '<script type="text/javascript" src="' + file + js_ext + '"></script>';
+            result = '<script type="text/javascript" src="' + file + JS_EXTENSION + '"></script>';
         } else {
             result = '<script type="text/javascript">';
-            result += '<![CDATA[';
-            result += this.getInlineContent(file + js_ext);
-            result += ']]>';
+            result += '//<![CDATA[';
+            result += this.getInlineContent(file + JS_EXTENSION, false, compress);
+            result += '//]]>';
             result += '</script>';
         }
         return result;
     };
 
-    ImportsHelper.prototype.injectCSS = function (file, inline) {
+    ImportsHelper.prototype.injectCSS = function(file, inline, compress) {
         var result;
         if (!inline) {
-            result = '<link rel="stylesheet" type="text/css" href="' + file + css_ext + '" />';
+            result = '<link rel="stylesheet" type="text/css" href="' + file;
+            result += CSS_EXTENSION + '" />';
         } else {
             result = '<style type="text/css">';
-            result += '/* inline file ' + file + css_ext + '*/';
-            result += this.getInlineContent(file + css_ext);
+            result += '/* inline file ' + file + CSS_EXTENSION + '*/';
+            result += this.getInlineContent(file + CSS_EXTENSION, true, compress);
             result += '</style>';
         }
         return result;
     };
 
     /**
-     * 
-     * 
-     * 
+     *
+     *
+     *
      */
-    ImportsHelper.prototype.render = function (file, inline, options) {
-        var data, result,
+    ImportsHelper.prototype.render = function(file, inline, minify, opts) {
+        var valid, result,
+            useinline = false,
+            useminify = false,
+            options = opts || {},
             args = mustacher.hasOptions(arguments);
+
         if (!args || args.length < 2) {
             throw new Error('missing arguments');
         }
-        if (args.length < 3) {
-            options = inline;
-            inline = false;
+
+        useinline = inline;
+        valid = !isboolean(inline) && args.length < 3;
+        if (!valid) {
+            useminify = false;
+            useinline = false;
+            options = inline || {};
         }
-        inline = inline || false;
+
+        useminify = minify;
+        valid = !isboolean(minify) && args.length < 4;
+        if (!valid) {
+            useminify = false;
+            options = minify || {};
+        }
+
         switch (options.name) {
         case '$js':
-            result = this.injectJS(file, inline);
+            result = this.injectJS(file, useinline, useminify);
             break;
         case '$css':
-            result = this.injectCSS(file, inline);
+            result = this.injectCSS(file, useinline, useminify);
             break;
         }
         return new Handlebars.SafeString(result);
     };
 
     /**
-     * 
-     * 
-     * 
+     *
+     *
+     *
      */
-    ImportsHelper.prototype.register = function () {
+    ImportsHelper.prototype.register = function() {
         Handlebars.registerHelper('$js', this.render.bind(this));
         Handlebars.registerHelper('$css', this.render.bind(this));
     };
