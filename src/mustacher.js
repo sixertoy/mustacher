9189ecd06cddf96a3c006cfbac493d70e0096785
sixertoy <sixertoy.github@gmail.com>
2020-01-15 18:48:18 -0300

    update to es6

--- src/mustacher.js
+++ src/mustacher.js
@@ -1,95 +1,85 @@
-/*jslint indent: 4, nomen: true, plusplus: true */
-/*globals require, module, process, __dirname, console */
-(function() {
-  'use strict';
+let OPTIONS = {};
+let IS_REGISTERED = false;
+const DEFAULTS = {
+  cwd: process.cwd(),
+  delimiter: {
+    ldim: '{{',
+    rdim: '}}',
+  },
+  partials: {
+    depth: 2,
+    ext: '.hbs',
+    src: 'partials/',
+  },
+};
+const HELPERS = [
+  'livereload',
+  'conditions',
+  'timestamp',
+  'include',
+  'literal',
+  'imports',
+  'repeat',
+  'random',
+  'image',
+  'equal',
+];
+// requires
+const path = require('path');
+const merge = require('lodash.merge');
+const handlebars = require('handlebars');
+const isempty = require('lodash.isempty');
+const toarray = require('lodash.toarray');
+const isstring = require('lodash.isstring');
+const isplainobject = require('lodash.isplainobject');
 
-  var // variables
-    mustacher,
-    _options = {},
-    _isRegistered = false,
-    _defaults = {
-      cwd: process.cwd(),
-      delimiter: {
-        ldim: '{{',
-        rdim: '}}',
-      },
-      partials: {
-        depth: 2,
-        ext: '.hbs',
-        src: 'partials/',
-      },
-    },
-    _helpers = [
-      'livereload',
-      'conditions',
-      'timestamp',
-      'include',
-      'literal',
-      'imports',
-      'repeat',
-      'random',
-      'image',
-      'equal',
-    ],
-    // requires
-    path = require('path'),
-    merge = require('lodash.merge'),
-    handlebars = require('handlebars'),
-    isempty = require('lodash.isempty'),
-    toarray = require('lodash.toarray'),
-    isstring = require('lodash.isstring'),
-    isplainobject = require('lodash.isplainobject');
+const mustacher = (...args) => {
+  const [str, context, options] = args;
+  if (args.length < 1 || !isstring(str)) {
+    throw new Error('missing arguments');
+  }
+  if (isempty(str)) return str;
+  if (!IS_REGISTERED) mustacher.register();
+  const nextContext = context || {};
+  OPTIONS = merge(DEFAULTS, options || {}, nextContext);
+  try {
+    const template = handlebars.compile(str);
+    const output = template(nextContext, { data: { root: OPTIONS } });
+    return output;
+  } catch (e) {
+    // console.log(e.stack);
+    throw new Error('Handlebars compile error');
+  }
+};
 
-  mustacher = function(str, context, options) {
-    var template;
-    if (arguments.length < 1 || !isstring(str)) {
-      throw new Error('missing arguments');
-    }
-    if (isempty(str)) {
-      return str;
-    }
-    if (!_isRegistered) {
-      mustacher.register();
-    }
-    context = context || {};
-    _options = merge(_defaults, options || {}, context);
-    try {
-      template = handlebars.compile(str);
-      return template(context, { data: { root: _options } });
-    } catch (e) {
-      // console.log(e.stack);
-      throw new Error('Handlebars compile error');
-    }
-  };
+mustacher.register = helpers => {
+  (helpers || HELPERS).forEach(name => {
+    const helperPath = path.join(__dirname, 'helpers', name);
+    // eslint-disable-next-line import/no-dynamic-require
+    const HelperPrototype = require(helperPath);
+    const helper = new HelperPrototype();
+    helper.register();
+  });
+  IS_REGISTERED = true;
+};
 
-  mustacher.register = function(helpers) {
-    var helper, HelperPrototype;
-    (helpers || _helpers).forEach(function(name) {
-      HelperPrototype = require(path.join(__dirname, 'helpers', name));
-      helper = new HelperPrototype();
-      helper.register();
-    });
-    _isRegistered = true;
-  };
+mustacher.options = () => {
+  return OPTIONS;
+};
 
-  mustacher.options = function() {
-    return _options;
-  };
+/**
+ * Mustacher
+ *
+ */
+mustacher.hasOptions = args => {
+  if (arguments.length < 1 || isempty(args)) {
+    return false;
+  }
+  args = toarray(args);
+  const optionArgument = args[args.length - 1];
+  const isObject = isplainobject(optionArgument);
+  const hasPropertyName = Boolean(isObject && optionArgument.name);
+  return hasPropertyName ? args : false;
+};
 
-  /**
-   * Mustacher
-   *
-   */
-  mustacher.hasOptions = function(args) {
-    if (arguments.length < 1 || isempty(args)) {
-      return false;
-    }
-    args = toarray(args);
-    return isplainobject(args[args.length - 1]) &&
-      args[args.length - 1].hasOwnProperty('name')
-      ? args
-      : false;
-  };
-
-  module.exports = mustacher;
-})();
+module.exports = mustacher;
